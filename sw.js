b1d3a6134825adc940b7dd71f5b798133254a5d0
hani200hoo-blip <hani200hoo@gmail.com>
2025-11-02 08:51:32 +0300

    Add service worker for caching and fetching

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'chtel-cache-v1';
+const FILES = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
+  );
+});
+commit message:add sw.js commit
